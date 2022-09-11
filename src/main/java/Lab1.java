bbbb1b8c021f85a699a0cc5c98297f60c75f518d
vadimkononenko <vadiskon@gmail.com>
2022-09-11 22:41:14 +0300

    Feature: added check for negative parameters

--- src/main/java/Lab1.java
+++ src/main/java/Lab1.java
@@ -16,14 +16,17 @@ public class Lab1 {
     }
 
     static int solver(int a, int b, int n, int m) {
+        if (a < 0 || b < 0 || n < 0 || m < 0)
+            throw new IllegalStateException("Enter positive value(s)");
+
         int resultSum = 0;
 
         for (short i = (short) a; i <= n; i++) {
             for (short j = (short) b; j <= m; j++) {
                 resultSum += (i % j)/(i + j);
-                System.out.println(resultSum);
             }
         }
+
         return resultSum;
     }
 }
